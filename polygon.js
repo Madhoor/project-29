32fbabce90174e2586002285f3736a3dc5bed948
Madhoor <79635992+Madhoor@users.noreply.github.com>
2021-04-25 17:59:10 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,24 @@
+class polygon{
+    constructor(x, y, r,  angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, r,  options);
+        this.r = r;
+       
+
+        this.image = loadImage("polygon.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, 50, 50);
+        pop();
+      }
+}
